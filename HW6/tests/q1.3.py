86512196627402ab08df9978f1c0f08205c3ffee
Jes Parker <jes.prkr@gmail.com>
2021-04-17 21:28:20 -0700

    upload HW6 v1.1

---
+++ HW6/tests/q1.3.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1.3',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> assert np.isclose(i[Idir],62.8334);\n>>> assert np.isclose(i[Imul],59.2169)\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
